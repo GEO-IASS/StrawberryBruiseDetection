bbd523cf66a2dbb9ef2382d8110146f05876892c
JieLiang@laptop <liangjiecn@outlook.com>
2016-09-02 09:39:44 +1000

    add funtions for the spectral response extraction and dataset construction

--- label_bruise.m
+++ label_bruise.m
@@ -1,7 +1,6 @@
 function rect=label_bruise(filename,varargin)
-addpath(cd);
 % label the bruise region on the strawberry
-datacube = Load_Spec(filename);
+datacube = importdata(filename);
 img = datacube(:,:,31);
 imshow(img, []);
 if isempty(varargin)
@@ -10,8 +9,12 @@ else rect = importdata (varargin{1});
 end
 hold on;
 rectangle('Position',rect,'EdgeColor','r', 'LineWidth',2);
+F = getframe;
+savename = regexprep(filename,'_r.mat','_labeled.png', 'ignorecase');
+imwrite(F.cdata, savename);
+
 BW=zeros(size(img,1),size(img,2));
 BW(round(rect(2)):round(rect(2)+rect(4)),round(rect(1)):round(rect(1)+rect(3)))=1;
-savename = regexprep(filename,'.hdr','_label.png', 'ignorecase');
+savename = regexprep(filename,'_r.mat','_groundTruth.png', 'ignorecase');
 imwrite(BW,savename);
 
